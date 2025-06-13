f05498a8c9dd9fc0d24d9a02ac27e39791c33d9e
dreamcurson <dreamcurson@outlook.com>
2025-06-13 08:29:42 -0400

    modification de fonctions.php (script)

--- functions.php
+++ functions.php
@@ -33,7 +33,22 @@ add_filter( 'get_custom_logo', 'custom_logo_class' );
 function theme_tp_enqueue_styles() { 
 wp_enqueue_style('normalize', get_template_directory_uri() . 'normalize.css'); 
 wp_enqueue_style('main-style', get_stylesheet_uri()); 
+
+    function mon_theme_ajouter_scripts() {
+    $script_path = get_template_directory() . '/script/checkbox.js';
+    $script_url  = get_template_directory_uri() . '/script/checkbox.js';
+
+    wp_enqueue_script(
+        'mon-script',
+        $script_url,
+        array(),
+        filemtime($script_path),
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'mon_theme_ajouter_scripts');
 } 
+
 add_action('wp_enqueue_scripts', 'theme_tp_enqueue_styles');
 
 ?>
