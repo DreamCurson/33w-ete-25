59690acac19d2b737ad1bb1bbe24aa53a08ddeb5
dreamcurson <dreamcurson@outlook.com>
2025-06-20 10:58:39 -0400

    Ajout gabarit formulaire

---
+++ gabarit/formulaire.php
@@ -0,0 +1,55 @@
+<section class="formulaire">
+  <div class="formulaire__section">
+    <form class="formulaire__form">
+      <div class="formulaire__field">
+        <label for="nom" class="formulaire__label">Nom :</label>
+        <input
+          type="text"
+          id="nom"
+          name="nom"
+          class="formulaire__input"
+          placeholder="Écrivez votre nom"
+          required
+        />
+      </div>
+
+      <div class="formulaire__field">
+        <label for="prenom" class="formulaire__label">Prénom :</label>
+        <input
+          type="text"
+          id="prenom"
+          name="prenom"
+          class="formulaire__input"
+          placeholder="Écrivez votre prénom"
+          required
+        />
+      </div>
+
+      <div class="formulaire__field">
+        <label for="courriel" class="formulaire__label">Courriel :</label>
+        <input
+          type="email"
+          id="courriel"
+          name="courriel"
+          class="formulaire__input"
+          placeholder="Écrivez votre courriel"
+          required
+        />
+      </div>
+
+      <div class="formulaire__field">
+        <label for="telephone" class="formulaire__label">Téléphone :</label>
+        <input
+          type="tel"
+          id="telephone"
+          name="telephone"
+          class="formulaire__input"
+          placeholder="Écrivez votre téléphone"
+          required
+        />
+      </div>
+
+      <button type="submit" class="formulaire__bouton">S'INSCRIRE</button>
+    </form>
+  </div>
+</section>
