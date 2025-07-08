a63936753732063bfd80d2a68ff70d7f459f8c44
dreamcurson <dreamcurson@outlook.com>
2025-07-08 09:29:17 -0400

    Ajout de single.php et suppression de index

--- single.php
+++ single.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="style.css" />
 <?php get_header(); ?>
-
 <section class="populaire">
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <?php get_template_part('gabarit/populaire_seul'); ?>
