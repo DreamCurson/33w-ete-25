fb3c0765a38688e0df45607cc6c75093e167dcc6
dreamcurson <dreamcurson@outlook.com>
2025-07-09 16:12:21 -0400

    Ajout des descriptions de category + scss

--- category.php
+++ category.php
@@ -1,14 +1,15 @@
-<link rel="stylesheet" href="style.css" />
 <?php get_header(); ?>
+
+<section class="categorie__description">
+    <h1 class="categorie__titre"><?php single_cat_title(); ?></h1>
+    <p class="categorie__texte"><?php echo category_description(); ?></p>
+</section>
 
 <section class="populaire">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <?php get_template_part('gabarit/populaire'); ?>
+        <?php get_template_part('gabarit/populaire'); ?>
     <?php endwhile; endif; ?>
 </section>
 
-
 <?php get_template_part('gabarit/galerie'); ?>
-
 <?php get_footer(); ?>
-<script src="script/checkbox.js"></script>
